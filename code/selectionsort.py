fd995c9e07ccb8c71757db8a6a9ecef4d985bb8d
picoasis <374917085@qq.com>
2021-11-19 11:26:38 +0800

    update:选择排序法--方案2，理顺逻辑，简化语句

--- code/selectionsort.py
+++ code/selectionsort.py
@@ -26,20 +26,17 @@
 # print( selectionSort(testArr) )
 # # [-3, 1, 3, 5, 9, 10]
 
-# 方案2:不新建arr，直接交换元素顺序
+# 方案2:不新建arr，当发现更小的元素时，直接交换元素顺序
 # 时间复杂度：两次遍历 O(n^2); 空间复杂度：交换时创建中转变量O(n)
+
+def selectionsort(arr):
+    for i in range(0,len(arr)):
+        for j in range(i+1,len(arr)):
+            if arr[i] > arr[j]:
+                arr[i],arr[j] = arr[j],arr[i]
+                # print(arr)
+    return arr
+# test
 nums = [4, 1, 5, 10, -1, 9, 3, 2, 13, 7]
- 
-count = len(nums)   # count等于nums的长
-for i in range(count-1):
-    min = i
-    for j in range(i+1, count):  # 将剩下的进行遍历，遍历到count
-        if nums[min] > nums[j]:
-            min = j
-    t = 0
-    if min != i:    # 若最小值不等于i，进行交换
-        nums[i],nums[min] = nums[min],nums[i]
-        # t = nums[i]
-        # nums[i] = nums[min]
-        # nums[min] = t
-print(nums)
+t = selectionsort(nums)
+print(t)
